c832830148b7b0b0d68213f87e3d970109ff115b
gvsheva 3405066+gvsheva@users.noreply.github.com <g.shevchenko@infingame.com>
2025-04-02 09:49:53 +0300

    init

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,51 @@
+class Key {
+    private signature: number = Math.random();
+
+    public getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {}
+
+    public getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    public comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log("Person came in");
+        } else {
+            console.log("Door is closed");
+        }
+    }
+
+    public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    public openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log("Door is opened");
+        } else {
+            console.log("Wrong key");
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -7,5 +55,4 @@ house.openDoor(person.getKey());
 
 house.comeIn(person);
 
-
-export {};
+export { Key, Person, House, MyHouse };
